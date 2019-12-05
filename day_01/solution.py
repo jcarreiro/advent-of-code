77de9cd04af9bf69c4fae5c7baffc41738b6847c
Jason Carreiro <jcarreiro@fb.com>
2019-12-05 14:46:16 -0500

    Solved part 2 of day 1

--- day_01/solution.py
+++ day_01/solution.py
@@ -6,6 +6,13 @@ import math
 def fuel_required(m):
     return math.floor(m / 3.0) - 2
 
+# This version takes into account that fuel has mass, too (see below).
+def fuel_required_part2(m):
+    df = fuel_required(m)
+    if df <= 0:
+        return 0
+    return df + fuel_required_part2(df)
+
 def solve(input):
     total = 0
     for m in input:
@@ -14,9 +21,21 @@ def solve(input):
         print(f"fuel_required({m:07}) = {fuel_for_component}, new total is {total:07}.")
     return total
 
-def solve2(input):
+# In part 1 we just need to compute the total fuel needed based on the mass of
+# each module (see the file "input" for the masses).
+def solve_part1(input):
     # This originally didn't work since I left out the initializer!
     return functools.reduce(lambda t, x: t + fuel_required(x), input, 0)
+
+# In part 2 we need to take into account that fuel has mass too! We need to add
+# more fuel to account of the mass of the fuel, and so on, until the amount of
+# fuel we need to add is <= 0 for a module.
+#
+# Note that we can't just work from the total fuel computed in part 1 -- we'll
+# overestimate the fuel needed in that case, since the problem asks for us to
+# do this per-module!
+def solve_part2(input):
+    return functools.reduce(lambda t, x: t + fuel_required_part2(x), input, 0)
 
 if __name__=="__main__":
     # For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
@@ -28,5 +47,9 @@ if __name__=="__main__":
     print("-----------")
 
     with open("input", "r") as f:
-        output = solve2((int(l) for l in f))
-        print(f"The answer is {output}.")
+        part1_answer = solve_part1((int(l) for l in f))
+        print(f"The answer to part 1 is {part1_answer}.")
+
+        f.seek(0)  # it'd be more efficient to compute both totals at once...
+        part2_answer = solve_part2((int(l) for l in f))
+        print(f"The answer to part 2 is {part2_answer}.")
